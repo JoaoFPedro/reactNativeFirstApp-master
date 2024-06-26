d48233a70468272c2eb557e6b964bfaa8a3ed32f
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-26 18:17:25 -0300

    feat button css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Button } from "react-native";
+import { Button, View } from "react-native";
+import styles from "./styles";
 
 interface FilterButtonProps {
   handleApplyFilter: () => void;
 }
 
 const ButtonComponent = ({ handleApplyFilter }: FilterButtonProps) => {
-  return <Button title="Filter" onPress={handleApplyFilter} />;
+  return (
+    <View style={styles.button}>
+      <Button title="Filter" onPress={handleApplyFilter} />
+    </View>
+  );
 };
 
 export default ButtonComponent;
