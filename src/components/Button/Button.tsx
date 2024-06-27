429457303b88e608c3dfff74fc478b2f2344c500
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-27 17:00:02 -0300

    trynag to change the button logic

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,7 +8,13 @@ interface FilterButtonProps {
 
 const ButtonComponent = ({ handleApplyFilter }: FilterButtonProps) => {
   return (
-    <View style={styles.button}>
+    <View style={styles.container}>
+      {/* <TextInput
+        style={styles.input}
+        placeholder="Filtre por nome"
+        value={inputFilter}
+        onChangeText={(value) => setInputFilter(value)}
+      /> */}
       <Button title="Filter" onPress={handleApplyFilter} />
     </View>
   );
