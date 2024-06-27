b0c8526522ae43016720fcc82404e25f7af5bc90
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-27 20:52:32 -0300

    eslint

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,6 +2,7 @@ import { Image, Text, View } from 'react-native';
 
 import { Game } from '../../services/games.Services';
 import styles from './styles';
+import React = require('react');
 
 interface GameCardProps {
   game: Game;
@@ -14,12 +15,12 @@ function GameCard ({game}: GameCardProps) {
     
     return (
         <View style={styles.card}>
-            <Image style={styles.image} source={{ uri: game.imagem }}/>
+            <Image style={styles.image} source={{ uri: game.imagem }} />
             <Text style={styles.title}>{game.nome}</Text>
             <Text style={styles.data}>{`Plataforma: ${game.plataforma}`}</Text>
 
         </View>
     )
-};
+}
 
 export default GameCard;
