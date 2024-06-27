eda6f8d8df84faeba80e567d9330c53747c2cb84
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-27 16:59:35 -0300

    changing the logic

--- App.tsx
+++ App.tsx
@@ -1,57 +1,53 @@
-import { useEffect, useState } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
 
 import React from "react";
 import styles from "./AppStyle";
-import Footer from "./src/components/footer/footer";
-import Header from "./src/components/header";
-import PlatformChecklist from "./src/components/PlataformChecklist/PlataformCheckList";
-import GameList from "./src/pages/GameList";
-import getGames, { Game } from "./src/services/games.Services";
+import HomeScreen from "./src/components/HomeScreen/HomeScreen";
+import { Game } from "./src/services/games.Services";
 
 export default function App() {
   const [games, setGames] = useState<Game[]>([]);
-  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
-  const [platforms, setPlatforms] = useState<string[]>([]);
+  // const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState<Error | null>(null);
+  // const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+  // const [platforms, setPlatforms] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        const data = await getGames();
-        setGames(data);
-        setFilteredGames(data);
-        setPlatforms(getUniquePlataform(data));
-      } catch (error) {
-        setError(error as Error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // const getUniquePlataform = useCallback((games: Game[]) => {
+  //   const plataform = games.map((game) => game.plataforma).flat();
+  //   console.log(plataform);
+  //   return [...new Set(plataform)];
+  // }, []);
 
-    fetchGames();
-  }, []);
+  // const fetchGames = useCallback(async () => {
+  //   try {
+  //     const data = await getGames();
+  //     setGames(data);
+  //     setFilteredGames(data);
+  //     setPlatforms(getUniquePlataform(data));
+  //   } catch (error) {
+  //     setError(error as Error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, [getUniquePlataform]);
+  // useEffect(() => {
+  //   fetchGames();
+  // }, [fetchGames]);
 
-  const getUniquePlataform = (games: Game[]) => {
-    const plataform = games.map((game) => game.plataforma).flat();
-    console.log(plataform);
-    return [...new Set(plataform)];
-  };
+  // const applyFilter = (filterText: string) => {
+  //   if (filterText === "") {
+  //     setGames(games);
+  //   } else {
+  //     const lowerCasedFilter = filterText.toLocaleLowerCase();
+  //     const filtered = games.filter((game) =>
+  //       game.nome.toLocaleLowerCase().includes(lowerCasedFilter)
+  //     );
 
-  const applyFilter = (filterText: string) => {
-    if (filterText === "") {
-      setFilteredGames(games);
-    } else {
-      const lowerCasedFilter = filterText.toLocaleLowerCase();
-      const filtered = games.filter((game) =>
-        game.nome.toLocaleLowerCase().includes(lowerCasedFilter)
-      );
-
-      setFilteredGames(filtered);
-    }
-  };
+  //     setGames(filtered);
+  //   }
+  // };
 
   // const handleFilterChange = (filterText: string) => {
   //   const lowerCasedFilter = filterText.toLocaleLowerCase();
@@ -64,44 +60,37 @@ export default function App() {
   //   setFilteredGames(filtered);
   // };
 
-  const handlePlatformChange = (platform: string) => {
-    const updatedPlatforms = selectedPlatforms.includes(platform)
-      ? selectedPlatforms.filter((p) => p !== platform)
-      : [...selectedPlatforms, platform];
+  // const handlePlatformChange = (platform: string) => {
+  //   const updatedPlatforms = selectedPlatforms.includes(platform)
+  //     ? selectedPlatforms.filter((p) => p !== platform)
+  //     : [...selectedPlatforms, platform];
 
-    setSelectedPlatforms(updatedPlatforms);
-    if (!updatedPlatforms.length) {
-      setFilteredGames(games);
-      return;
-    }
-    const filtered = filteredGames.filter((game) => {
-      const gameWithPlataform = game.plataforma.find((platformItem) => {
-        return updatedPlatforms.includes(platformItem);
-      });
+  //   setSelectedPlatforms(updatedPlatforms);
+  //   if (!updatedPlatforms.length) {
+  //     setFilteredGames(games);
+  //     return;
+  //   }
+  //   const filtered = filteredGames.filter((game) => {
+  //     const gameWithPlataform = game.plataforma.find((platformItem) => {
+  //       return updatedPlatforms.includes(platformItem);
+  //     });
 
-      return !!gameWithPlataform;
-    });
+  //     return !!gameWithPlataform;
+  //   });
 
-    setFilteredGames(filtered);
-  };
-  if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
-  }
+  //   setFilteredGames(filtered);
+  // };
+  // if (loading) {
+  //   return <ActivityIndicator size="large" color="#0000ff" />;
+  // }
 
-  if (error) {
-    return <Text>Erro: {error.message}</Text>;
-  }
+  // if (error) {
+  //   return <Text>Erro: {error.message}</Text>;
+  // }
 
   return (
     <View style={styles.container}>
-      <Header applyFilter={applyFilter} />
-      <PlatformChecklist
-        platforms={platforms}
-        selectedPlatforms={selectedPlatforms}
-        onPlatformChange={handlePlatformChange}
-      />
-      <GameList games={filteredGames} />
-      <Footer />
+      <HomeScreen />
     </View>
   );
 }
--- AppStyle.ts
+++ AppStyle.ts
@@ -1,12 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
 });
 
 export default styles;
