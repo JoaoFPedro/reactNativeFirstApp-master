d59771a31f3c5944247501b325ead219de3d87db
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-28 19:08:41 -0300

    feat: add game list using game context to GameList component

--- src/contexts/gameContext.tsx
+++ src/contexts/gameContext.tsx
@@ -1,8 +1,18 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import React from "react";
+import getGames, { Game } from "../services/games.Services";
 
-const GameContext = createContext({
+interface GameContextType {
+  games: Game[] | null;
+  applyFilter: () => void;
+  filteredGames: Game []
+}
+
+const GameContext = createContext<GameContextType>({
   games: null,
-});
+  applyFilter:() => { console.log('applyFilter is called');},
+  filteredGames: []
+})
 
 const useGameContext = () => {
   return useContext(GameContext);
@@ -11,10 +21,42 @@ const useGameContext = () => {
 const GameContextProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const [games, setGames] = useState(null);
+ 
 
+  const [games, setGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+  const [platforms, setPlatforms] = useState<string[]>([]);
+
+  const getUniquePlataform = useCallback((games: Game[]) => {
+    const plataform = games.map((game) => game.plataforma).flat();
+    console.log(plataform);
+    return [...new Set(plataform)];
+  }, []);
+
+  const fetchGames = useCallback(async () => {
+    try {
+      const data = await getGames();
+      setGames(data);
+      setFilteredGames(data);
+      setPlatforms(getUniquePlataform(data));
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  }, [getUniquePlataform]);
+  useEffect(() => {
+    fetchGames();
+  }, [fetchGames]);
+
+  const applyFilter =() =>{
+    console.log('HELLOU WORLD')
+  }
   return (
-    <GameContext.Provider value={{ games }}>{children}</GameContext.Provider>
+    <GameContext.Provider value={{ applyFilter, filteredGames, games }}>{children}</GameContext.Provider>
   );
 };
 export { GameContext, GameContextProvider, useGameContext };
